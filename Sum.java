8f68d2270973ba86bcda4bd59f700ab6f7dfa9a7
viveksinghal32454 <viveksinghal32454@gmail.com>
2022-05-04 21:24:33 +0530

    Code Uploaded

---
+++ Sum.java
@@ -0,0 +1,11 @@
+
+public class Sum {
+	public static void main(String[] args) {
+		int a , b , sum;
+		a=4;
+		b=3;
+		sum = a+b;
+		System.out.println("The sum of "+a+" and "+b+" is "+sum);
+	}
+
+}
